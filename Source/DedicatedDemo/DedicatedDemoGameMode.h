e474e778628603a04822934acd8b05dee39a6be2
Nick H <NicolasAbouHaidar@Gmail.com>
2024-05-28 14:40:13 -0400

    Changed the way server operates, now idles and waits for lobbyId to be sent. Added server widget to connect to RSM tool websocket server

--- Source/DedicatedDemo/DedicatedDemoGameMode.h
+++ Source/DedicatedDemo/DedicatedDemoGameMode.h
@@ -5,16 +5,24 @@
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "S2SRTTComms.h"
+#include <BCClientPlugin/Private/WinWebSocketBase.h>
 #include "DedicatedDemoGameMode.generated.h"
+
 
 
 DECLARE_DYNAMIC_DELEGATE_OneParam(FS2SCallbackDelegate, const FString&, OutputString);
 DECLARE_DYNAMIC_DELEGATE_OneParam(FS2SRTTCallbackDelegate, const FString&, JsonData);
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FRSMSocketClosed);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FRSMSocketConnected);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRSMSocketReceiveData, const TArray<uint8>&, data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRSMSocketReceiveMessage, const FString&, data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRSMSocketConnectError, const FString&, error);
+
 DECLARE_LOG_CATEGORY_EXTERN(DedicatedServerLog, Log, All);
 
 UCLASS(minimalapi)
-class ADedicatedDemoGameMode : public AGameModeBase
+class ADedicatedDemoGameMode : public AGameModeBase, public IWinWebSocketBaseCallbacks
 {
 	GENERATED_BODY()
 
@@ -23,6 +31,12 @@ protected:
 
 public:
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "BrainCloud S2S")
+		void InitS2S(const FString& appId, const FString& serverName, const FString& serverSecret);
+
+	UFUNCTION(BlueprintCallable, Category = "BrainCloud Room Server Manager Tool")
+		void ConnectToRSMTool();
 
 	UFUNCTION(BlueprintCallable, Category = "BrainCloud S2S")
 		void S2SRequest(const FString& requestJson, FS2SCallbackDelegate Callback);
@@ -44,13 +58,31 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Dedicated Server")
 		void ShutdownServer();
+		
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "BrainCloud S2S")
+		void OnLobbyAssigned();
+
+	void OnLobbyAssigned_Implementation();
 
 	UPROPERTY(BlueprintReadOnly)
 		FString lobbyId;
 
 private:
 
+	void setupWebSocket(const FString& in_url);
+
+	void OnConnectError(const FString& error) override;
+	void OnClosed() override;
+	void OnConnectComplete() override;
+	void OnReceiveData(const TArray<uint8>& data) override;
+	
+	void OnDataProcessed(const FString& message);
+	
+
 	US2SRTTComms *pS2S;
+
+	UWinWebSocketBase* m_connectedSocket;
 
 	FString serverName;
 	FString serverSecret;
